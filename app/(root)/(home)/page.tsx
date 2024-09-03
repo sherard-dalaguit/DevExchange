dab0fef3828a00e2ea28893cfa6c5476fce71688
sherard_dalaguit <sherard.dalaguit@gmail.com>
2024-09-02 18:05:51 -0700

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">I love Jesus</h1>
+      <UserButton />
     </div>
   )
 }
