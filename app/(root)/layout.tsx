72bbe9a69bee6b3cfde8a94081ac193fcf6b80b1
sherard_dalaguit <sherard.dalaguit@gmail.com>
2024-09-03 08:53:59 -0700

    implemented navbar (global search, mobile nav, logo)

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from '@/components/shared/navbar/Navbar'
+import React from 'react'
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main className="background-light850_dark100 relative">
+        <div className="flex">
+            <Navbar />
+            <section className="flex min-h-screen flex-1 flex-col 
+            px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+                <div className="mx-auto w-full max-w-5xl">
+                    {children}
+                </div>
+            </section>
+            right side bar
+        </div>
+
+        Toaster
+    </main>
+  )
+}
+
+export default Layout
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState, useEffect } from "react"
 
 interface ThemeContextType {
     mode: string;
-    setMode: (mode:string) => void;
+    setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
@@ -13,12 +13,14 @@ export function ThemeProvider({ children }: {children: React.ReactNode}) {
     const [mode, setMode] = useState('');
 
     const handleThemeChange = () => {
-        if (mode === 'dark') {
-            setMode('light')
-            document.documentElement.classList.add('light')
-        } else {
+        if (localStorage.theme === 'dark' ||
+            (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
             setMode('dark')
             document.documentElement.classList.add('dark')
+        } else {
+            setMode('light')
+            document.documentElement.classList.remove('dark')
         }
     }
 
