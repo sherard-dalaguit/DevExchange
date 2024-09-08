90aa9d00ac5f2bc64b72e05e983bcb480fa2b522
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-07 23:52:03 -0700

    Add answer handling system
    Implement a comprehensive answer system including new components, database models, and server actions. This includes the addition of `AllAnswers` and `Answer` components, the `answer.model.ts` for database schema, and relevant server-side actions for creating and fetching answers.

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,47 @@
+"use server"
+
+import Answer from "@/database/answer.model"
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types"
+import { connectToDatabase } from "../mongoose"
+import { revalidatePath } from "next/cache"
+import Question from "@/database/question.model"
+import User from "@/database/user.model"
+
+export async function createAnswer(params: CreateAnswerParams) {
+    try {
+        connectToDatabase()
+
+        const { content, author, question, path } = params
+
+        const newAnswer = await Answer.create({ content, author, question });
+
+        // Add the answer to the question's answer array
+        await Question.findByIdAndUpdate(question, {
+            $push: { answers: newAnswer._id }
+        })
+
+        // TODO: Add interaction...
+        
+        revalidatePath(path)
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+    try {
+        connectToDatabase();
+
+        const { questionId } = params
+
+        const answers = await Answer.find({ question: questionId })
+            .populate("author", "_id clerkId name picture")
+            .sort({ createdAt: -1 })
+
+        return { answers }
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -100,12 +100,3 @@ export async function getAllUsers(params: GetAllUsersParams) {
         throw error;
     }
 }
-
-// export async function getAllUsers(params: GetAllUsersParams) {
-//     try {
-//         connectToDatabase();
-//     } catch (error) {
-//         console.log(error);
-//         throw error;
-//     }
-// }
