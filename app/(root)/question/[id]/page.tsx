dcc18be6728c1035771ae09d547e48103b003eb4
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-09 13:38:37 -0700

    created a page for each tags' details

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -7,11 +7,12 @@ import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action';
 import { getUserById } from '@/lib/actions/user.action';
 import { formatBigNumber, getTimestamp } from '@/lib/utils';
+import { URLProps } from '@/types';
 import { auth } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function Page({ params, searchParams }) {
+export default async function Page({ params, searchParams }: URLProps) {
     const result = await getQuestionById({ questionId: params.id });
     const { userId: clerkId } = auth();
 
