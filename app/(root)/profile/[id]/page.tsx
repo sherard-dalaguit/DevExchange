545330d0651bd53a988fe980bbd13d1688a5a3b0
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-09 23:47:42 -0700

    created Stats section for profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -74,7 +74,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
                 </div>
             </div>
 
-            <Stats />
+            <Stats 
+                totalQuestions={userInfo.totalQuestions}
+                totalAnswers={userInfo.totalAnswers}
+            />
 
             <div className="mt-10 flex gap-10">
                 <Tabs defaultValue="top-posts" className="flex-1">
