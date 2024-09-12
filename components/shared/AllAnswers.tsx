4bb2b02d46f975d07ed5acd025273204e32fd48b
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-12 14:54:43 -0700

    Add pagination to AllAnswers component
    Introduce Pagination component to handle navigation through answers. This change enhances user experience by allowing easy access to different pages of answers. The pagination logic is based on the current page and the availability of further answers.

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { getTimestamp } from '@/lib/utils';
 import ParseHTML from './ParseHTML';
 import Votes from './Votes';
+import Pagination from './Pagination';
 
 interface Props {
     questionId: string;
@@ -69,6 +70,13 @@ const AllAnswers = async ({ questionId, userId, totalAnswers, page, filter }: Pr
                         <ParseHTML data={answer.content} />
                     </article>
                 ))}
+            </div>
+
+            <div className="mt-10">
+                <Pagination 
+                    pageNumber={page ? +page : 1} 
+                    isNext={result.isNextAnswer}    
+                />
             </div>
         </div>
     )
