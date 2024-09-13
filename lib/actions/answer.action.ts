90583ff636f5056fe9e0b66dcdc07ef168ce03a6
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-13 12:53:58 -0700

    Add reputation updates and interactions for answers
    Implemented logic to update user reputation based on answer creation and vote actions. Created interaction records when an answer is posted to track user activities.

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../mongoose"
 import { revalidatePath } from "next/cache"
 import Question from "@/database/question.model"
 import User from "@/database/user.model"
+import Interaction from "@/database/interaction.model"
 
 export async function createAnswer(params: CreateAnswerParams) {
     try {
@@ -16,11 +17,21 @@ export async function createAnswer(params: CreateAnswerParams) {
         const newAnswer = await Answer.create({ content, author, question });
 
         // Add the answer to the question's answer array
-        await Question.findByIdAndUpdate(question, {
+        const questionObject = await Question.findByIdAndUpdate(question, {
             $push: { answers: newAnswer._id }
         })
 
-        // TODO: Add interaction...
+        await Interaction.create({
+            user: author,
+            action: "answer",
+            question,
+            answer: newAnswer._id,
+            tags: questionObject.tags
+        })
+
+        await User.findByIdAndUpdate(author, {
+            $inc: { reputation: 10 }
+        })
         
         revalidatePath(path)
     } catch (error) {
@@ -96,6 +107,14 @@ export async function upvoteAnswer(params: AnswerVoteParams) {
             throw new Error("Answer not found");
         }
 
+        await User.findByIdAndUpdate(userId, {
+            $inc: { reputation: hasupVoted ? -2 : 2}
+        })
+
+        await User.findByIdAndUpdate(answer.author, {
+            $inc: { reputation: hasupVoted ? -10 : 10}
+        })
+
         revalidatePath(path)
     } catch (error) {
 
@@ -128,6 +147,14 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
         if (!answer) {
             throw new Error("Answer not found");
         }
+
+        await User.findByIdAndUpdate(userId, {
+            $inc: { reputation: hasdownVoted ? -2 : 2}
+        })
+
+        await User.findByIdAndUpdate(answer.author, {
+            $inc: { reputation: hasdownVoted ? -10 : 10}
+        })
 
         revalidatePath(path)
     } catch (error) {
