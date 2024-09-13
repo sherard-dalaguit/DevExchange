791dc0e76e5a619ea7a996563d69cd5945907845
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-13 11:56:40 -0700

    Enhance global search functionality and improve UX
    Integrated the `globalSearch` function for fetching search results. Updated `GlobalResult.tsx` to handle dynamic link rendering and improved `GlobalSearch.tsx` with outside click detection to close the search dropdown when clicking outside.

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -6,6 +6,8 @@ import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import GlobalFilters from './GlobalFilters';
+import { globalSearch } from '@/lib/actions/general.action';
+import { set } from 'mongoose';
 
 const GlobalResult = () => {
     const searchParams = useSearchParams();
@@ -26,7 +28,9 @@ const GlobalResult = () => {
             setIsLoading(true)
 
             try {
+                const res = await globalSearch({ query: global, type })
 
+                setResult(JSON.parse(res))
             } catch(error) {
                 console.log(error)
                 throw error
@@ -34,10 +38,25 @@ const GlobalResult = () => {
                 setIsLoading(false)
             }
         }
+
+        if (global) {
+            fetchResult();
+        }
     }, [global, type])
 
     const renderLink = (type: string, id: string) => {
-        return '/'
+        switch (type) {
+            case 'question':
+                return `/question/${id}`;
+            case 'answer':
+                return `/question/${id}`;
+            case 'user':
+                return `/user/${id}`;
+            case 'tag':
+                return `/tags/${id}`;
+            default:
+                return '/'
+        }
     }
 
     return (
@@ -61,7 +80,7 @@ const GlobalResult = () => {
                         {result.length > 0 ? (
                             result.map((item: any, index: number) => (
                                 <Link
-                                    href={renderLink('type', 'id')}
+                                    href={renderLink(item.type, item.id)}
                                     key={item.type + item.id + index}
                                     className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:hover:bg-dark-500/50"
                                 >
