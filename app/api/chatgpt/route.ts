7e2f47ce424e4d9ee06bce8325143acb4bc64805
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-14 16:59:12 -0700

    idk hopefully now it works

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -1,27 +1,34 @@
 import { NextResponse } from "next/server";
-import { OpenAI } from 'openai';
-require('dotenv').config();
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
 
 export const POST = async (request: Request) => {
-    const { question } = await request.json();
+  const { question } = await request.json();
 
-    try {
-        const completion = await openai.chat.completions.create({
-            model: "gpt-4o-mini",
-            messages: [
-                {"role": "system", "content": "You are a knowledgeable assistant that provides quality information."},
-                {"role": "user", "content": `Tell me ${question}`},
-            ]
-        });
-        
-        const reply = completion.choices[0].message.content;
-        console.log(reply)
-        return NextResponse.json({ reply })
-    } catch (error: any) {
-        return NextResponse.json({error: error.message})
-    }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-4o-mini',
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a knowlegeable assistant that provides quality information.'
+          }, {
+            role: 'user',
+            content: `Tell me ${question}`
+          }
+        ]
+      })
+    })
+
+    const responseData = await response.json();
+    const reply = responseData.choices[0].message.content;
+
+    return NextResponse.json({ reply })
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message })
+  }
 }
